dc06e01febe18216b7b79c1b729f6371a21be239
Gigi Bachtel <gbachtel@ucsc.edu>
2019-03-04 00:22:52 -0800

    Added Unity Project
    Currently has a title screen that transitions to the game and an animated sprite

---
+++ Lunacueva Legado/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
