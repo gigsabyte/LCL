cc4edd3b76fded0fc3fd871cfc447bb0d23b2cd9
Gigi Bachtel <gbachtel@ucsc.edu>
2019-03-05 18:20:38 -0800

    Added basic tilemap, camera movement, collision
    \o/

--- Lunacueva Legado/Assets/Scripts/GetMousePos.cs
+++ Lunacueva Legado/Assets/Scripts/GetMousePos.cs
@@ -6,10 +6,13 @@ public class GetMousePos : MonoBehaviour
 {
     [SerializeField]
     private Camera cam;
+
+    [SerializeField]
+    private GameObject moth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -18,5 +21,8 @@ public class GetMousePos : MonoBehaviour
         Vector3 pos = Input.mousePosition;
         //pos.z = 1;
         transform.position = Camera.main.ScreenToWorldPoint(pos);
+        pos = moth.transform.position;
+        pos.z = -10;
+        cam.gameObject.transform.position = Vector3.Lerp(cam.gameObject.transform.position, pos, Time.deltaTime);
     }
 }
